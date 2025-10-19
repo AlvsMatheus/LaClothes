1420223221dc5a4a75f605dd3b59ee029a495733
Matheus Alves <matheussilvaemac@gmail.com>
2025-10-19 13:20:28 -0300

    cred fixed as null and produtoscolecaoref turned a dependency

--- src/app/hero/ItemsSection.tsx
+++ src/app/hero/ItemsSection.tsx
@@ -1,52 +1,50 @@
-'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { db } from '../config/config-firebase'
-import { getDocs, collection } from 'firebase/firestore'
-import { ArrowLeft, ArrowRight } from 'lucide-react'
-import Link from 'next/link'
-import ItemsCard from './ItemsCard'
-import type { ProdutosTipos, ItemsSectionProps } from '../types/index'
-gsap.registerPlugin(ScrollTrigger)
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { db } from "../config/config-firebase";
+import { getDocs, collection } from "firebase/firestore";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Link from "next/link";
+import ItemsCard from "./ItemsCard";
+import type { ProdutosTipos, ItemsSectionProps } from "../types/index";
+gsap.registerPlugin(ScrollTrigger);
 
-
-const ItemsSection = ({label, category}: ItemsSectionProps) => {
-  const [produtos, setProdutos] = useState<ProdutosTipos[]>([])
-  const produtoscolecaoRef = collection(db, "products")
+const ItemsSection = ({ label, category }: ItemsSectionProps) => {
+  const [produtos, setProdutos] = useState<ProdutosTipos[]>([]);
+  const produtoscolecaoRef = collection(db, "products");
   const scrollRef = useRef<HTMLDivElement>(null);
-  const [underline, setUnderline] = useState<boolean>(false)
+  const [underline, setUnderline] = useState<boolean>(false);
 
   useEffect(() => {
     const getProdutos = async () => {
-    try{
-    const data = await getDocs(produtoscolecaoRef)
-    const filteredData: ProdutosTipos[] = data.docs.map((doc) => ({
-      ...doc.data() as ProdutosTipos,
-       id: doc.id
-      }));
+      try {
+        const data = await getDocs(produtoscolecaoRef);
+        const filteredData: ProdutosTipos[] = data.docs.map((doc) => ({
+          ...(doc.data() as ProdutosTipos),
+          id: doc.id,
+        }));
 
-      if (category === 'em-alta') {
-        setProdutos(filteredData.filter(item => item.emAlta === true))
-      } else {
-        const produtosFiltrados = filteredData.filter(item => item.category === category)
-        setProdutos(produtosFiltrados)
+        if (category === "em-alta") {
+          setProdutos(filteredData.filter((item) => item.emAlta === true));
+        } else {
+          const produtosFiltrados = filteredData.filter(
+            (item) => item.category === category
+          );
+          setProdutos(produtosFiltrados);
+        }
+      } catch (err) {
+        console.error(err);
       }
-    } catch(err) {
-      console.error(err)
-    }
-  }
-  getProdutos();
-  }, [category])
+    };
+    getProdutos();
+  }, [category, produtoscolecaoRef]);
 
-  
-
-  const sectionRef = useRef<HTMLDivElement>(null)
-
+  const sectionRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const element = sectionRef.current
-    if (!element) return
+    const element = sectionRef.current;
+    if (!element) return;
 
     const ctx = gsap.context(() => {
       gsap.fromTo(
@@ -56,47 +54,44 @@ const ItemsSection = ({label, category}: ItemsSectionProps) => {
           opacity: 1,
           x: 0,
           duration: 1,
-          ease: 'power3.out',
+          ease: "power3.out",
           scrollTrigger: {
             trigger: element,
-            start: 'top 80%',
-            end: 'top 30%',
-            toggleActions: 'play none none none',
+            start: "top 80%",
+            end: "top 30%",
+            toggleActions: "play none none none",
             scrub: false,
             markers: false,
             onEnter: () => {
-                setUnderline(true)
-            }
-          }
+              setUnderline(true);
+            },
+          },
         }
-      )
-    }, element)
-    return () => ctx.revert()
+      );
+    }, element);
+    return () => ctx.revert();
   }, []);
 
   useEffect(() => {
-  if (!produtos.length) return;
+    if (!produtos.length) return;
 
-  const ctx = gsap.context(() => {
-    gsap.from(".card-item", {
-      opacity: 0,
-      x: 30,
-      duration: 0.6,
-      stagger: 0.2, // cada card demora 0.2s a mais
-      ease: "power3.out",
-      scrollTrigger: {
-        trigger: sectionRef.current,
-        start: "top 80%",
-        toggleActions: "play none none none",
-      }
-    });
-  }, sectionRef); // usa sectionRef como contexto
+    const ctx = gsap.context(() => {
+      gsap.from(".card-item", {
+        opacity: 0,
+        x: 30,
+        duration: 0.6,
+        stagger: 0.2, // cada card demora 0.2s a mais
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      });
+    }, sectionRef); // usa sectionRef como contexto
 
-  return () => ctx.revert();
-}, [produtos]);
-
-
- 
+    return () => ctx.revert();
+  }, [produtos]);
 
   const scroll = (direction: "left" | "right") => {
     if (!scrollRef.current) return;
@@ -108,66 +103,68 @@ const ItemsSection = ({label, category}: ItemsSectionProps) => {
   };
 
   return (
-    <article
-      ref={sectionRef}
-      className='flex flex-col min-h-screen py-10'
-    >
-      <section className='flex justify-start items-center w-full'>
-        <div className='flex flex-col gap-10 justify-center items-center ps-10 md:ps-20'>
-          <div className='flex flex-col gap-2'>
-            <h1 className='text-[#e8e8e8] font-bold text-5xl'>
-              {label}
-            </h1>
-            <div className={`h-1 bg-[#e8e8e8] transition-all ease-in-out duration-1000 ${underline ? 'w-[100%]' : 'w-[0%]'}`} />
+    <article ref={sectionRef} className="flex flex-col min-h-screen py-10">
+      <section className="flex justify-start items-center w-full">
+        <div className="flex flex-col gap-10 justify-center items-center ps-10 md:ps-20">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-[#e8e8e8] font-bold text-5xl">{label}</h1>
+            <div
+              className={`h-1 bg-[#e8e8e8] transition-all ease-in-out duration-1000 ${
+                underline ? "w-[100%]" : "w-[0%]"
+              }`}
+            />
           </div>
-          <div className='flex w-full'>
+          <div className="flex w-full">
             <a
-              className='relative group w-30 border-2 border-sky-100 rounded overflow-hidden'
+              className="relative group w-30 border-2 border-sky-100 rounded overflow-hidden"
               href="#"
             >
-              <h1 className='relative z-10 text-white group-hover:text-black text-[18px] font-extralight leading-5 px-3 py-2 transition-all ease-in-out duration-500'>
+              <h1 className="relative z-10 text-white group-hover:text-black text-[18px] font-extralight leading-5 px-3 py-2 transition-all ease-in-out duration-500">
                 VER TODOS
               </h1>
-              <div 
-              className='absolute top-0 left-0 w-[0%] group-hover:w-[100%]
-               h-full bg-amber-50 transition-all ease-in-out duration-500' />
+              <div
+                className="absolute top-0 left-0 w-[0%] group-hover:w-[100%]
+               h-full bg-amber-50 transition-all ease-in-out duration-500"
+              />
             </a>
           </div>
         </div>
       </section>
       <section>
         {/* move to right and left */}
-        <section className='hidden lg:flex justify-end w-full h-20'>
-          <div className='flex justify-between items-center p-6 h-full w-40 text-white'>
+        <section className="hidden lg:flex justify-end w-full h-20">
+          <div className="flex justify-between items-center p-6 h-full w-40 text-white">
             <button
-            className='group hover:cursor-pointer'
-            onClick={() => scroll("left")}>
-              <ArrowLeft 
-               height={40}
-               width={40}
-               className='group-hover:scale-120 transition-all ease-in-out '/>
+              className="group hover:cursor-pointer"
+              onClick={() => scroll("left")}
+            >
+              <ArrowLeft
+                height={40}
+                width={40}
+                className="group-hover:scale-120 transition-all ease-in-out "
+              />
             </button>
             <button
-            className='group hover:cursor-pointer'
-            onClick={() => scroll("right")}>
-              <ArrowRight 
-              height={40} 
-              width={40} 
-              className='group-hover:scale-120 transition-all ease-in-out'/>
+              className="group hover:cursor-pointer"
+              onClick={() => scroll("right")}
+            >
+              <ArrowRight
+                height={40}
+                width={40}
+                className="group-hover:scale-120 transition-all ease-in-out"
+              />
             </button>
           </div>
         </section>
-        <section 
-        ref={scrollRef}
-        className='flex items-center w-full overflow-auto scrollbar-custom backdrop-blur-sm h-140'>
+        <section
+          ref={scrollRef}
+          className="flex items-center w-full overflow-auto scrollbar-custom backdrop-blur-sm h-140"
+        >
           <div>
-            <section className='w-[2000px] grid grid-cols-20 gap-90'>
+            <section className="w-[2000px] grid grid-cols-20 gap-90">
               {produtos.map((item) => (
-                <Link
-                href={`/buy/${item.id}`}
-                key={item.id}
-                >
-                  <ItemsCard item={item}/>
+                <Link href={`/buy/${item.id}`} key={item.id}>
+                  <ItemsCard item={item} />
                 </Link>
               ))}
             </section>
@@ -175,7 +172,7 @@ const ItemsSection = ({label, category}: ItemsSectionProps) => {
         </section>
       </section>
     </article>
-  )
-}
+  );
+};
 
-export default ItemsSection
+export default ItemsSection;
--- src/app/sign/Sign.tsx
+++ src/app/sign/Sign.tsx
@@ -2,7 +2,11 @@
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { auth, googleProvider } from "../config/config-firebase";
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup  } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { logoGoogle } from "@/assets";
 import Image from "next/image";
@@ -15,156 +19,169 @@ const Sign = () => {
 
   const signIn = async (e: React.MouseEvent) => {
     e.preventDefault();
-    try{
-      let cred
+    try {
+      let cred = null;
       if (mode === "Login") {
         cred = await signInWithEmailAndPassword(auth, email, password);
       } else {
         cred = await createUserWithEmailAndPassword(auth, email, password);
       }
-      console.log("UID do user:", cred.user.uid)
-      router.push("/user");
-    } catch(err) {
-      console.error(err)
-    }
-  }
 
-  const signInGoogle = async (e :React.MouseEvent) => {
-    e.preventDefault();
-    let cred
-    try{
-    cred = await signInWithPopup(auth, googleProvider);
-    } catch(err) {
-      console.error(err)
+      if (cred) {
+        console.log("UID do user:", cred.user.uid);
+        router.push("/user");
+      }
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
+
+  const signInGoogle = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    let cred;
+    try {
+      cred = await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const [mode, setMode] = useState<"Login" | "Register">("Login");
-  
 
   const containerRef = useRef(null);
   const [isAnimationComplete, setIsAnimationComplete] = useState(false);
 
   useEffect(() => {
-  const ctx = gsap.context(() => {
-    gsap.from([".login-title", ".form-element"], {
-      opacity: 0,
-      filter: "blur(10px)",
-      x: -50,
-      duration: 1,
-      ease: "power2.out",
-      stagger: 0.2,
-      onComplete: () => {
-        setIsAnimationComplete(true);
-      }
+    const ctx = gsap.context(() => {
+      gsap.from([".login-title", ".form-element"], {
+        opacity: 0,
+        filter: "blur(10px)",
+        x: -50,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.2,
+        onComplete: () => {
+          setIsAnimationComplete(true);
+        },
+      });
     });
-  }); 
 
-  
-  return () => ctx.revert();
-
-
-}, []);
+    return () => ctx.revert();
+  }, []);
   return (
     <header ref={containerRef}>
       <div className="flex justify-center items-center h-screen md:h-[200dvh] lg:h-screen w-full bg-[url('/hero-background.jpg')]  bg-top lg:bg-center xl:bg-top">
         <div className="backdrop-blur-md bg-white/10 shadow-lg h-110 w-90 md:w-120 lg:h-125 lg:w-90 rounded-2xl border border-white/40 transition-all duration-500 ease-in-out">
           <div className="grid grid-cols-1 grid-rows-[12%_88%] gap-5 md:gap-2  lg:gap-8 h-full w-full p-5 md:p-2 lg:p-10 rounded-2xl">
             <div className="flex justify-center">
-              <h1 className="text-3xl font-roboto text-white login-title">Login</h1>
+              <h1 className="text-3xl font-roboto text-white login-title">
+                Login
+              </h1>
             </div>
-            <form
-            className=""
-            action="">
-             <div className="flex flex-col h-full w-full items-center gap-7 lg:gap-4">
-               <div className="flex justify-center w-full form-element">
-                <label
-                 className="sr-only"
-                 htmlFor="iemail">Email</label>
-                <input 
-                required
-                autoFocus
-                type="text" 
-                id="iemail" 
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email" 
-                className="w-50 border rounded-4xl p-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent" />
+            <form className="" action="">
+              <div className="flex flex-col h-full w-full items-center gap-7 lg:gap-4">
+                <div className="flex justify-center w-full form-element">
+                  <label className="sr-only" htmlFor="iemail">
+                    Email
+                  </label>
+                  <input
+                    required
+                    autoFocus
+                    type="text"
+                    id="iemail"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    className="w-50 border rounded-4xl p-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+                  />
                 </div>
                 <div className="flex flex-col items-center justify-center form-element">
-                  <label 
-                  className="sr-only"
-                  htmlFor="ipassword">Password</label>
-                  <input 
-                  required
-                  type="password" 
-                  id="ipassword"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Password" 
-                  className=" w-50 border rounded-4xl p-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent " />
+                  <label className="sr-only" htmlFor="ipassword">
+                    Password
+                  </label>
+                  <input
+                    required
+                    type="password"
+                    id="ipassword"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    className=" w-50 border rounded-4xl p-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent "
+                  />
                   <div className="flex justify-end w-full mt-1">
-                    <a
-                    href="#"
-                    className="text-gray-300 text-sm">
-                    forgot password?
+                    <a href="#" className="text-gray-300 text-sm">
+                      forgot password?
                     </a>
                   </div>
                 </div>
                 <div className="flex items-center justify-center flex-col gap-2">
-                  <div className={`group bg-gradient-to-bl  hover:bg-gradient-to-r from-black/30 to-blue-400 w-20 text-center p-2 rounded-3xl my-0 lg:my-6 form-element hover:bg-blue-400 hover:cursor-pointer  ${isAnimationComplete ? 'transition-all duration-500 ease-in-out' : ''}`}>
+                  <div
+                    className={`group bg-gradient-to-bl  hover:bg-gradient-to-r from-black/30 to-blue-400 w-20 text-center p-2 rounded-3xl my-0 lg:my-6 form-element hover:bg-blue-400 hover:cursor-pointer  ${
+                      isAnimationComplete
+                        ? "transition-all duration-500 ease-in-out"
+                        : ""
+                    }`}
+                  >
                     <button
-                    onClick={signIn}
-                    className="text-white font-roboto font-bold group-hover:cursor-pointer group-hover:text-blue-200 font-roboto ">
+                      onClick={signIn}
+                      className="text-white font-roboto font-bold group-hover:cursor-pointer group-hover:text-blue-200 font-roboto "
+                    >
                       {mode === "Login" ? "Login" : "Register"}
                     </button>
                   </div>
-                    {mode === "Login" && (
-                      <div className="form-element">
-                        <button
+                  {mode === "Login" && (
+                    <div className="form-element">
+                      <button
                         onClick={signInGoogle}
-                        className="text-white font-bold group-hover:cursor-pointer group-hover:text-blue-200 font-roboto ">
-                          <Image 
+                        className="text-white font-bold group-hover:cursor-pointer group-hover:text-blue-200 font-roboto "
+                      >
+                        <Image
                           className="cursor-pointer"
                           src={logoGoogle.imgPath}
                           alt="Logo Do Google"
-                          width={40} 
+                          width={40}
                           height={40}
-                          />
-                        </button>
-                      </div>
-                    )}
+                        />
+                      </button>
+                    </div>
+                  )}
                 </div>
                 <div className="flex flex-col items-center gap-2 w-full form-element">
                   <div className=" flex items-center gap-2">
-                      <label
+                    <label
                       className="text-white text-nowrap text-sm font-roboto"
-                      htmlFor="iremember">Remember me</label>
-                      <input
+                      htmlFor="iremember"
+                    >
+                      Remember me
+                    </label>
+                    <input
                       id="iremember"
                       name="remember"
                       className="checked"
-                      type="checkbox" />
+                      type="checkbox"
+                    />
                   </div>
                   <div>
                     <button
-                    type="button"
-                    onClick={() => setMode(mode === "Login" ? "Register" : "Login")}
-                    className="text-blue-600 text-sm cursor-pointer font-bold"
+                      type="button"
+                      onClick={() =>
+                        setMode(mode === "Login" ? "Register" : "Login")
+                      }
+                      className="text-blue-600 text-sm cursor-pointer font-bold"
                     >
                       {mode === "Login" ? `Criar conta` : `Já tenho conta`}
                     </button>
                   </div>
                 </div>
-             </div>
+              </div>
             </form>
           </div>
         </div>
-      </div> 
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Sign
+export default Sign;
