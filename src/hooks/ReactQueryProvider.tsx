8579965f4732a35c6724a0211a415cd90de3d1a8
Matheus Alves <matheussilvaemac@gmail.com>
2026-03-29 11:01:32 -0300

    backend folder deleted

--- src/hooks/ReactQueryProvider.tsx
+++ src/hooks/ReactQueryProvider.tsx
@@ -3,8 +3,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
-export default function ReactQueryProvider({ children }: { children: ReactNode }) {
-
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const [client] = useState(() => new QueryClient());
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
